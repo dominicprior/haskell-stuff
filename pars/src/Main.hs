d5c1e0d39c124f5a4513bc1863887656527a7538
Dominic Prior <dominicprior@yahoo.co.uk>
2016-11-26 20:53:13 +0000

    Add expressions

--- pars/src/Main.hs
+++ pars/src/Main.hs
@@ -57,6 +57,46 @@ perlIf = do
   char ')' >> spaces >> char '{'
   return $ "if " ++ concat toks ++ ":"
 
+perlDollarVar :: P st String
+perlDollarVar = char '$' >> perlVar
+
+perlMy :: P st String
+perlMy = string "my" >> spaces >> return ""
+
+perlParen :: P st String
+perlParen = do
+  char '(' >> spaces
+  expr <- perlExpr
+  spaces >> char ')'
+  return $ "(" ++ expr ++ ")"
+
+perlVar :: P st String
+perlVar = many1 $ oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "_"
+
+perlOp :: P st String
+perlOp = many1 $ oneOf "%^&*-+=.|<>~/"
+
+perlExpr :: P st String
+perlExpr = do
+  t1 <- perlTerm
+  spaces
+  m <- many $ do
+    op <- perlOp
+    spaces
+    t2 <- perlTerm
+    spaces
+    return $ op ++ " " ++ t2
+  return $ t1 ++ concat m
+
+perlTerm :: P st String
+perlTerm = perlLeaf <|> perlParen
+
+perlLeaf :: P st String
+perlLeaf = perlDollarVar <|> perlVar
+
+perlBracket :: P st String
+perlBracket = (:[]) <$> oneOf "[]"
+
 perlSub :: P st String
 perlSub = do
   string "sub" >> spaces
@@ -66,31 +106,6 @@ perlSub = do
   args <- perlDollarVar `sepBy1` (char ',' >> spaces)
   char ')' >> spaces >> char '=' >> spaces >> string "@_;\n"
   return $ "def " ++ name ++ "(" ++ intercalate ", " args ++ "):\n"
-
-perlDollarVar :: P st String
-perlDollarVar = char '$' >> perlVar
-
-perlMy :: P st String
-perlMy = string "my" >> spaces >> return ""
-
-perlParen :: P st String
-perlParen = do
-  char '('
-  toks <- perlTokens
-  char ')'
-  return $ "(" ++ concat toks ++ ")"
-
-perlVar :: P st String
-perlVar = many1 $ oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "_"
-
-perlOp :: P st String
-perlOp = many1 $ oneOf "%^&*-+=.|<>~/"
-
---perlExpr :: P st String
---perlExpr = 
-
-perlBracket :: P st String
-perlBracket = (:[]) <$> oneOf "[]"
 
 t p = parse p ""
 fromRight (Right x) = x
